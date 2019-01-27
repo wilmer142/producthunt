2b8f6c0fcd39affcc3a9b8939507a2d8ff146d01
wilmer142 <wilmercollege@gmail.com>
2019-01-27 17:23:35 -0500

    Create POST route /products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   #get '/products/index', to: 'products#index'
-  #get '/products/new', to: 'products#new'
+  #get '/products/new', to: 'products#new_path'
+  #post 'products/create', to: 'products#create'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
